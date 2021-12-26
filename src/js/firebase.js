a28e3ff354cf12f14d027999fd814e19e38e9420
Zolotarova-Nataliia <zolotarova.nataliia@gmail.com>
2021-12-26 12:23:31 +0200

    remove firebase version

--- src/js/firebase.js
+++
@@ -1,9 +0,0 @@
-import './modules/components/page-switch-firebase';
-import './modules/components/render-movie-modal-firebase';
-import { movieService } from './modules/components/movie-service-firebase';
-import './modules/components/team-modal';
-import './modules/components/anchor-btn';
-import './modules/components/form-firebase';
-// import './vendors/_firebase';
-
-movieService.renderMarkupAtHomePage();
